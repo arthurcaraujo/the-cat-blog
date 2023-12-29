c20b68834a3e18d15024e39f28353f765ab9da2f
arthurcaraujo <arthur-lagamar@hotmail.com>
2023-12-29 16:12:04 -0300

    Set up gh-pages and split the app content into different pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ArticleComponent } from './pages/article/article.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TopicComponent } from './pages/topic/topic.component';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        ArticleComponent,
+        CommonModule,
+        HomeComponent,
+        RouterOutlet,
+        TopicComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'the-cat-blog';
+    title = 'the-cat-blog';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ArticleComponent } from './pages/article/article.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TopicComponent } from './pages/topic/topic.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
+    {
+        path: ":topic",
+        component: TopicComponent
+    },
+    {
+        path: ":topic/:article",
+        component: ArticleComponent
+    }
+];
